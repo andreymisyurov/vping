969900928d657000e0b3d92f57d4e3e0c6cc14cc
ANDREY MISYUROV <andrey.misyrov@gmail.com>
2026-04-20 02:29:16 +0500

    feat: register virtual ethernet netdev

--- vping.c
+++ vping.c
@@ -4,12 +4,14 @@
  *         on an IPv4 address configured via procfs.
  */
 
+#include <linux/etherdevice.h>
 #include <linux/fs.h>
 #include <linux/in.h>
 #include <linux/inet.h>
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/netdevice.h>
 #include <linux/proc_fs.h>
 #include <linux/string.h>
 #include <linux/uaccess.h>
@@ -27,6 +29,7 @@ union v4_addr {
 
 static __be32 vping_ip;
 static struct proc_dir_entry *vping_proc;
+static struct net_device *vping_dev;
 
 static ssize_t vping_proc_read(struct file *file, char __user *buf,
 			       size_t len, loff_t *offset)
@@ -74,6 +77,39 @@ static ssize_t vping_proc_write(struct file *file, const char __user *buf,
 	return len;
 }
 
+static int vping_ndo_open(struct net_device *dev)
+{
+	netif_start_queue(dev);
+	return 0;
+}
+
+static int vping_ndo_stop(struct net_device *dev)
+{
+	netif_stop_queue(dev);
+	return 0;
+}
+
+static netdev_tx_t vping_ndo_start_xmit(struct sk_buff *skb,
+				       struct net_device *dev)
+{
+	dev_kfree_skb(skb);
+	return NETDEV_TX_OK;
+}
+
+static const struct net_device_ops vping_netdev_ops = {
+	.ndo_open	= vping_ndo_open,
+	.ndo_stop	= vping_ndo_stop,
+	.ndo_start_xmit	= vping_ndo_start_xmit,
+};
+
+static void vping_setup(struct net_device *dev)
+{
+	ether_setup(dev);
+	dev->netdev_ops = &vping_netdev_ops;
+	dev->flags |= IFF_NOARP;
+	eth_hw_addr_random(dev);
+}
+
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
 static const struct proc_ops vping_proc_ops = {
 	.proc_read  = vping_proc_read,
@@ -89,18 +125,43 @@ static const struct file_operations vping_proc_ops = {
 
 static int __init vping_init(void)
 {
+	int err;
+
 	vping_proc = proc_create(DRV_NAME, 0644, NULL, &vping_proc_ops);
 	if (!vping_proc) {
 		pr_err("%s: failed to create /proc/%s\n", DRV_NAME, DRV_NAME);
 		return -ENOMEM;
 	}
 
-	pr_info("%s: loaded, /proc/%s created\n", DRV_NAME, DRV_NAME);
+	vping_dev = alloc_netdev(0, "vping%d", NET_NAME_ENUM, vping_setup);
+	if (!vping_dev) {
+		pr_err("%s: alloc_netdev failed\n", DRV_NAME);
+		err = -ENOMEM;
+		goto err_proc;
+	}
+
+	err = register_netdev(vping_dev);
+	if (err) {
+		pr_err("%s: register_netdev failed: %d\n", DRV_NAME, err);
+		goto err_free;
+	}
+
+	pr_info("%s: loaded, /proc/%s and %s ready\n",
+		DRV_NAME, DRV_NAME, vping_dev->name);
 	return 0;
+
+err_free:
+	free_netdev(vping_dev);
+	vping_dev = NULL;
+err_proc:
+	proc_remove(vping_proc);
+	return err;
 }
 
 static void __exit vping_exit(void)
 {
+	unregister_netdev(vping_dev);
+	free_netdev(vping_dev);
 	proc_remove(vping_proc);
 	pr_info("%s: unloaded\n", DRV_NAME);
 }
@@ -110,5 +171,5 @@ module_exit(vping_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Andrey Misyurov <andrey.misyurov@gmail.com>");
-MODULE_DESCRIPTION("vping: configurable IPv4 address via /proc/vping (read/write)");
-MODULE_VERSION("0.2.0");
+MODULE_DESCRIPTION("vping: virtual netdev with IPv4 configured via /proc/vping");
+MODULE_VERSION("0.3.0");
